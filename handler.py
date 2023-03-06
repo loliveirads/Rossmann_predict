6aed378fb115fc00a32e4117dde0764129957b06
loliveirads <119435421+loliveirads@users.noreply.github.com>
2023-03-06 17:03:41 -0300

    Update handler.py

--- handler.py
+++ handler.py
@@ -1,48 +1,49 @@
-import os
-import pickle
 import pandas as pd
-from flask             import *
+import pickle
+import os
+
+from flask import Flask, request, Response
 from rossmann.Rossmann import Rossmann
-import xgboost as xgb
-import requests
+
 # loading model
-model = pickle.load( open( 'model/rossmann.pkl', 'rb') )
+model = pickle.load( open('model/rossmann.pkl', 'rb'))
 
-# initialize API
-app = Flask( __name__ )
+# iniciando API
+app = Flask( __name__)
 
-@app.route( '/rossmann/predict', methods=['POST'] )
+@app.route( '/rossmann/predict', methods=['POST'])
 def rossmann_predict():
     test_json = request.get_json()
-   
-    if test_json: # there is data
-        if isinstance( test_json, dict ): # unique example
-            test_raw = pd.DataFrame( test_json, index=[0] )
-            
-        else: # multiple example
-            test_raw = pd.DataFrame( test_json, columns=test_json[0].keys() )
-            
-        # Instantiate Rossmann class
+    
+    if test_json: # se tiver dados
+        if isinstance( test_json, dict): # único exemplo
+            test_raw = pd.DataFrame( test_json, index=[0])
+        
+        else: # múltiplos exemplos
+            test_raw = pd.DataFrame( test_json, columns=test_json[0].keys())
+        
+        # intanciar classe Rossmann
         pipeline = Rossmann()
         
         # data cleaning
         df1 = pipeline.data_cleaning( test_raw )
         
         # feature engineering
-        df2 = pipeline.feature_engineering( df1 )
+        df2 = pipeline.feature_engineering(df1)
         
         # data preparation
-        df3 = pipeline.data_preparation( df2 )
+        df3 = pipeline.data_preparation(df2)
         
         # prediction
-        df_response = pipeline.get_prediction( model, test_raw, df3 )
-        
+        df_response = pipeline.get_prediction(model, test_raw, df3)
+    
         return df_response
-        
-        
+    
     else:
-        return Reponse( '{}', status=200, mimetype='application/json' )
+        return Response('{}', status=200, mimetype='application/json')
+    
+    
 
 if __name__ == '__main__':
     port = os.environ.get('PORT', 5000)
-    app.run( '0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port )
